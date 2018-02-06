434d02005e950385efd8f2325a4744289290ac3a
hanguokai <han.guokai@gmail.com>
2018-02-06 08:08:15 +0800

    refactor logic to analyzer class and it's subclass.

---
+++ analyzer.js
@@ -0,0 +1,128 @@
+/**
+ * Analyzer base class for detecting document update time.
+ * Note: subclass need to impelment these methods:
+ *   async findCurrentPageLastModified() {}
+ *   async findEnPageLastModified() {}
+ *   getEnPageUrl() {}
+ */
+class Analyzer {
+  /**
+   * @return true if this page is English language.
+   * if it is English page, analyzer do nothing.
+   */
+  isEn() {
+    const lang = document.documentElement.lang;
+    if(!lang || lang === "en" || lang === "en-US") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async getHTTPLastModified(url) {
+    try {
+      const option = {
+        method: 'HEAD',
+        credentials: 'same-origin'
+      }
+      const response = await fetch(url, option);
+      if(response.ok && response.headers.has("Last-Modified")) {
+        // This is GMT timezone
+        return new Date(response.headers.get("Last-Modified"));
+      } else {
+        return null;
+      }
+    } catch(e) {
+      return null;
+    }
+  }
+
+   /**
+    * init an analyzer for current page.
+    */
+   static initAnalyzer() {
+     // for MDN
+     if(location.hostname === "developer.mozilla.org") {
+       return new MDN();
+     } else {
+       return new GoogleSites();
+     }
+   }
+}
+
+/**
+ * for MDN Web Docs
+ */
+class MDN extends Analyzer {
+  async findCurrentPageLastModified() {
+    return this.findMDNPageLastModified(document);
+  }
+
+  async findEnPageLastModified() {
+    const enURL = this.getEnPageUrl();
+    if(enURL) {
+      const response = await fetch(enURL);
+      const html = await response.text();
+      const enHTML = new DOMParser().parseFromString(html, 'text/html');
+      return this.findMDNPageLastModified(enHTML);
+    }
+    return null;
+  }
+
+  getEnPageUrl() {
+    // replace developer.mozilla.org/zh-CN/ to /en-US/
+    const enAlternate = document.head.querySelector("link[rel=alternate][hreflang=en-US]");
+    return enAlternate ? enAlternate.href : null;
+  }
+
+  // curent page and english page are the same method
+  findMDNPageLastModified(dom) {
+    // a time element
+    const lastUpdateTime = dom.querySelector(".contributors-sub time");
+    if(lastUpdateTime) {
+      return new Date(lastUpdateTime.dateTime);
+    }
+    return null;
+  }
+}
+
+/**
+ * for other Google Dev Sites
+ */
+class GoogleSites extends Analyzer {
+  async findCurrentPageLastModified() {
+    // for android dev site
+    // <meta itemprop="dateModified" content="2017-07-27T14:36:46.324130">
+    const meta = document.head.querySelector("meta[itemprop='dateModified'");
+    if(meta) {
+      return this.getDateAsUTC(meta.content);
+    }
+
+    // for firebase, google-dev, cloud, tensorflow
+    // <p itemprop="datePublished" content="2017-08-07T20:49:39.902150">
+    // This info maybe not in some pages.
+    const p = document.querySelector("p[itemprop='datePublished'");
+    if(p) {
+      return this.getDateAsUTC(p.getAttribute("content"));
+    }
+
+    // default use http header
+    return await this.getHTTPLastModified(document.location.href);
+  }
+
+  async findEnPageLastModified() {
+    // this is ok for supported google sites, don't analyze page content.
+    return await this.getHTTPLastModified(this.getEnPageUrl());
+  }
+
+  getEnPageUrl() {
+    const params = new URLSearchParams(location.search);
+    // set ?hl=en param
+    params.set('hl', 'en');
+    return `${location.pathname}?${params}`;
+  }
+
+  getDateAsUTC(datestr) {
+    return new Date(datestr + 'Z')
+  }
+}
--- contentScript.js
+++ contentScript.js
@@ -1,109 +1,18 @@
-function getLang() {
-  return document.documentElement.lang;
-}
-
-async function findCurrentPageLastModified() {
-  // for MDN
-  if(location.hostname === "developer.mozilla.org") {
-    return findMDNPageLastModified(document);
-  }
-
-  // for android dev site
-  // <meta itemprop="dateModified" content="2017-07-27T14:36:46.324130">
-  const meta = document.head.querySelector("meta[itemprop='dateModified'");
-  if(meta) {
-    return getDateAsUTC(meta.content);
-  }
-
-  // for firebase, google-dev, cloud, tensorflow
-  // <p itemprop="datePublished" content="2017-08-07T20:49:39.902150">
-  // This info maybe not in some pages.
-  const p = document.querySelector("p[itemprop='datePublished'");
-  if(p) {
-    return getDateAsUTC(p.getAttribute("content"));
-  }
-
-  return await getHTTPLastModified(document.location.href);
-}
-
-function getDateAsUTC(datestr) {
-  return new Date(datestr + 'Z')
-}
-
-async function findEnPageLastModified() {
-  // for MDN
-  if(location.hostname === "developer.mozilla.org") {
-    return findMDNEnPageLastModified();
-  }
-  // other google devsites
-  return await getHTTPLastModified(getEnPageUrl());
-}
-
-async function getHTTPLastModified(url) {
-  try {
-    const option = {
-      method: 'HEAD',
-      credentials: 'same-origin'
-    }
-    const response = await fetch(url, option);
-    if(response.ok && response.headers.has("Last-Modified")) {
-      // This is GMT timezone
-      return new Date(response.headers.get("Last-Modified"));
-    } else {
-      return null;
-    }
-  } catch(e) {
-    return null;
-  }
-}
-
-function getEnPageUrl() {
-  const params = new URLSearchParams(location.search);
-  // set ?hl=en param
-  params.set('hl', 'en');
-  return `${location.pathname}?${params}`;
-}
-
-function getMDNEnPageUrl() {
-  // replace developer.mozilla.org/zh-CN/ to /en-US/
-  const enAlternate = document.head.querySelector("link[rel=alternate][hreflang=en-US]");
-  return enAlternate ? enAlternate.href : null;
-}
-
-async function findMDNEnPageLastModified() {
-  const enURL = getMDNEnPageUrl();
-  if(enURL) {
-    const response = await fetch(enURL);
-    const html = await response.text();
-    const enHTML = new DOMParser().parseFromString(html, 'text/html');
-    return findMDNPageLastModified(enHTML);
-  }
-  return null;
-}
-
-function findMDNPageLastModified(dom) {
-  // a time element
-  const lastUpdateTime = dom.querySelector(".contributors-sub time");
-  if(lastUpdateTime) {
-    return new Date(lastUpdateTime.dateTime);
-  }
-  return null;
-}
-
+// global data object
 var pageData = null;
 
 async function init() {
-  const lang = getLang();
-  if(!lang || lang === "en" || lang === "en-US") {
+  const analyzer = Analyzer.initAnalyzer();
+  if(analyzer.isEn()) {
     return
   }
 
-  const curUpdateTime = await findCurrentPageLastModified();
+  const curUpdateTime = await analyzer.findCurrentPageLastModified();
   if(!curUpdateTime) {
     return
   }
 
-  const oriUpdateTime = await findEnPageLastModified();
+  const oriUpdateTime = await analyzer.findEnPageLastModified();
   if(!oriUpdateTime) {
     return
   }
@@ -111,20 +20,10 @@ async function init() {
   // console.log("curUpdateTime: " + curUpdateTime);
   // console.log("oriUpdateTime: " + oriUpdateTime);
 
-  let enURL;
-  if(location.hostname === "developer.mozilla.org") {
-    enURL = getMDNEnPageUrl();
-  } else {
-    enURL = location.protocol
-          + "//"
-          + location.hostname
-          + getEnPageUrl();
-  }
-
   pageData = {
     oriUpdateTime: oriUpdateTime.getTime(),
     curUpdateTime: curUpdateTime.getTime(),
-    enURL: enURL,
+    enURL: analyzer.getEnPageUrl(),
     curURL: document.location.href
   }
 
