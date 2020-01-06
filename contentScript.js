26514f7485817c33d228456efb3b59921f371f59
guokai <han.guokai@gmail.com>
2020-01-07 07:39:50 +0800

    add watchUrlChanged

--- contentScript.js
+++ contentScript.js
@@ -48,34 +48,41 @@ async function init() {
  * Show warning banner in the page.
  * This is the same logic with popup.js
  */
+var banner;
 function showBanner() {
   const diff = pageData.oriUpdateTime - pageData.curUpdateTime;
   if(diff <= 60*30*1000) { // up to date or less than 30min
     return;
   }
 
-  // container element
-  const container = document.createElement('div');
+  if(!banner || !document.body.contains(banner)) {
+    banner = document.createElement('div');
+    banner.style.cssText = "position:fixed; width: 314px; margin-left: -157px; top: 12%; left: 50%; color: blue; border: 2px solid red; background-color: yellow; z-index: 1000000; font-size: 12px; padding: 3px;";
+    // note element
+    const note = document.createElement('span');
+    banner.appendChild(note);
+    // close element
+    const close = document.createElement('span');
+    close.innerText = "[X]";
+    close.title = "close";
+    close.style.cssText = "cursor: pointer;";
+    close.onclick = function(){
+      banner.remove();
+      banner = null;
+    };
+    banner.appendChild(close);
+    document.body.appendChild(banner);
+  }
+
+  // update
   const oneDay = 3600*24*1000;
   const noteString = chrome.i18n.getMessage("note");
   if(diff >= oneDay) { // behind more than 1 days
     const days = Math.trunc(diff/oneDay);
-    container.innerHTML = noteString + chrome.i18n.getMessage("behindDays", [days, pageData.enURL]);
+    banner.firstElementChild.innerHTML = noteString + chrome.i18n.getMessage("behindDays", [days, pageData.enURL]);
   } else if(diff > 0) { // behind less than 1 day
-    container.innerHTML = noteString + chrome.i18n.getMessage("behindLessThanOneDay", [pageData.enURL]);
+    banner.firstElementChild.innerHTML = noteString + chrome.i18n.getMessage("behindLessThanOneDay", [pageData.enURL]);
   }
-  container.style.cssText = "position:fixed; width: 314px; margin-left: -157px; top: 12%; left: 50%; color: blue; border: 2px solid red; background-color: yellow; z-index: 1000000; font-size: 12px; padding: 3px;";
-  // close element
-  const close = document.createElement('span');
-  close.innerText = "[X]";
-  close.title = "close";
-  close.style.cssText = "cursor: pointer;";
-  close.onclick = function(){
-    close.parentElement.hidden = true;
-  };
-  container.appendChild(close);
-  // add to body
-  document.body.appendChild(container);
 }
 
 // reponse popup to get page info.
@@ -88,4 +95,29 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
+function watchUrlChanged() {
+  let metaURL = document.head.querySelector("meta[property='og:url']");
+  if(metaURL) {
+    let config = { childList: true };
+    let observer = new MutationObserver((list, observer) => {
+      for(const mutation of list) {
+        for(const node of mutation.addedNodes) {
+          if(node.getAttribute('property') === "og:url") {
+            console.log('url changed')
+            if(banner) {
+              banner.remove();
+              banner = null;
+              chrome.runtime.sendMessage({reset: true});
+            }
+            init();
+            return;
+          }
+        }
+      }
+    });
+    observer.observe(document.head, config);
+  }
+}
+
 init();
+watchUrlChanged();
--- eventPage.js
+++ eventPage.js
@@ -6,6 +6,11 @@ function show(request, sender, sendResponse) {
   const tabId = sender.tab.id;
   chrome.pageAction.show(tabId);
 
+  if(request.reset) {
+    chrome.pageAction.setIcon({tabId: tabId, path: 'oudated-128.png'});
+    return;
+  }
+
   const oneDay = 3600*24*1000;
   const diff = request.oriUpdateTime - request.curUpdateTime;
   if(diff <= 60*30*1000) { // up to date or less than 30min
